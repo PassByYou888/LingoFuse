e4f4809dd9952fcddecadfbd94bb614298888555
老张 <passbyyou888@outlook.com>
2026-09-07 11:45:26 +0800

    Create build_llm_service.ps1

---
+++ Py/build_llm_service.ps1
@@ -0,0 +1,8 @@
+﻿pyinstaller --onefile `
+    --paths . `
+    --collect-all llama_cpp `
+    --collect-all lingofuse `
+    --hidden-import llama_cpp `
+    --hidden-import lingofuse `
+    llm-service\llm_service.py
+	
